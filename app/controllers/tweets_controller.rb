1d8ec9bdc848aecd1ec8b33969cdeb25a8ef23c3
Aldo Utrera <Kugel85@gmail.com>
2013-01-23 18:48:23 -0600

    Sending the user to the view to print the name in the rss feed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,16 @@
 class TweetsController < ApplicationController
   def show
-    user   = User.find_by_name(params[:id])
-    client = Twitter::Client.new(
-      :oauth_token => user.oauth_token,
-      :oauth_token_secret => user.oauth_token_secret
-    )
-    @tweets = client.home_timeline
-    respond_to do |format|
-      format.html # index.html.erb
-      format.rss { render layout: false }
+    @user = User.find_by_name(params[:id])
+    if @user
+      client = Twitter::Client.new(
+        :oauth_token => @user.oauth_token,
+        :oauth_token_secret => @user.oauth_token_secret
+      )
+      @tweets = client.home_timeline
+      respond_to do |format|
+        format.html # index.html.erb
+        format.rss { render layout: false }
+      end
     end
   end
 
